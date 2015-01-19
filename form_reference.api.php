d36bc63710609f7634db64d992f6b944e99ec585
Oliver Polden <oliver.polden@capgemini.com>
2015-01-19 15:17:50 +0000

    Better definition and use of hook_form_reference().

---
+++ form_reference.api.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * Documentation for form_reference API.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Specify forms to be available to form_reference.
+ *
+ * Each form is specified using its form_id as the element key.
+ *
+ * As a minimum, name should be defined and appears in the field configuration.
+ *
+ * Optional:
+ *   - category - Category the form belongs in. 'Other' if not specified.
+ *   - wrapper - A wrapper function to be used instead of drupal_get_form().
+ *   - module - The module in which the form exists.
+ *   - file - The file in which the form exists.
+ *   - validation_element - Existence of this element in the loaded form will be
+ *     checked to validate that the form_id is valid.
+ *
+ * @return array
+ *   Array of form definitions.
+ */
+function hook_form_reference() {
+  $forms = array(
+    'user_login' => array(
+      'name' => t('User Login'),
+    ),
+    'page' => array(
+      'name' => t('Basic page'),
+      'category' => t('Core'), // Optional
+      'wrapper' => 'node_add', // Optional
+      'module' => 'node', // Optional
+      'file' => 'node.pages.inc', // Optional
+      'validation_element' => '#node_edit_form', // Optional
+    ),
+  );
+
+  return $forms;
+}
